b691c579de0f99365838fe86edbed661c4db3099
Martin Cicala <141734582+MEC1998@users.noreply.github.com>
2023-08-16 09:12:11 -0600

    Add files via upload

---
+++ ejercicios_complementarios.py
@@ -0,0 +1,65 @@
+#Punto 1
+numero1=25
+
+#Punto 2
+numero2=12.5
+
+#Punto 3
+suma=numero1+numero2
+print("La suma de los números es: ", suma)
+
+#Punto 4
+resta=(numero1-numero2)
+mult=(numero1*numero2)
+div=(numero1/numero2)
+print("La diferencia entre los números es: ", resta)
+print("El producto de los dos números es: ", mult)
+print("El cociente de los números es: ", div)
+
+#Punto 5
+nombre="Martín"
+
+#Punto 6
+precio=90.5
+
+#Punto 7
+descuento=0.25*precio
+
+#Punto 8
+precio_final=precio-descuento
+print("El producto tiene un valor de $", precio, ". Le aplicamos un descuento del 25%, asi que el valor final es de $", precio_final)
+
+#Punto 9
+cadena="Dale Zelda dale"
+
+#Punto 10
+longitud=len(cadena)
+print(cadena)
+print(longitud)
+
+#Punto 11
+precio=10.6
+print(int(precio))
+
+#Punto 12
+nombre="Martín"
+apellido="Cicala"
+nombre_completo=nombre+" "+apellido
+print(nombre_completo)
+
+#Punto 13
+edad=25
+edad+=5
+edad-=10
+print(edad)
+
+#Punto 14
+altura= 1.75
+altura*=4
+altura/=3
+print(altura)
+
+#Punto 15
+nombre="MARTÍN"
+print(nombre.lower())
+print(nombre.title())
